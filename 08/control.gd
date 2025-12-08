5f0d0704ba62adc89a1b71a9e8fb6c200ecae0f5
maxi99manuel99 <manuel.trageser789@gmail.com>
2025-12-08 15:12:17 +0100

    Optimizations with running top-k neighbors

--- 08/control.gd
+++ 08/control.gd
@@ -18,7 +18,6 @@ class Point:
 	func _to_string() -> String:
 		return str(position)
 		
-		
 class Distance:
 	var point1: Point
 	var point2: Point
@@ -28,37 +27,54 @@ class Distance:
 		self.point1 = point1_
 		self.point2 = point2_
 		self.distance = distance_
-
-	
+		
+		
+func _sort_by_distance(a: Distance, b: Distance):
+	return a.distance < b.distance	
 
 func _compute_distances(points: Array[Vector3], top_k: int = 5) -> void:
-	# store only the top 5 nearest neighbors for each point (works for this puzzle, found by testing :D)
 	var n_points = len(points)
 	for point in points:
 		_point_objs.append(Point.new(point))
 
 	for i in n_points:
-		var neighbors := []
-		for j in range(i + 1, n_points): 
+		var top_k_neighbors: Array = []
+		var max_idx_top_neighbors := -1
+		var max_distance_top_neighbors := -INF
+		
+		for j in range(i + 1, n_points):
 			var d = points[i].distance_squared_to(points[j])
+			var dist_obj = Distance.new(_point_objs[i], _point_objs[j], d)
+			
 			if top_k:
-				neighbors.append(Distance.new(_point_objs[i], _point_objs[j], d))
-			else: 
-				_distances.append(Distance.new(_point_objs[i], _point_objs[j], d))
+				# running top-k neighbors
+				if len(top_k_neighbors) < top_k:
+					top_k_neighbors.append(dist_obj)
+					if d > max_distance_top_neighbors:
+						max_distance_top_neighbors = d
+						max_idx_top_neighbors = len(top_k_neighbors) - 1
+				else:
+					if d < max_distance_top_neighbors:
+						top_k_neighbors[max_idx_top_neighbors] = dist_obj
+						max_distance_top_neighbors = top_k_neighbors[0].distance
+						max_idx_top_neighbors = 0
+						for idx in range(1, len(top_k_neighbors)):
+							if top_k_neighbors[idx].distance > max_distance_top_neighbors:
+								max_distance_top_neighbors = top_k_neighbors[idx].distance
+								max_idx_top_neighbors = idx
+			else:
+				_distances.append(dist_obj)
 		
 		if top_k:
-			neighbors.sort_custom(func(a,b): return a.distance < b.distance)
-			for n in neighbors.slice(0, min(top_k, neighbors.size())):
+			for n in top_k_neighbors:
 				_distances.append(n)
 
-func _sort_by_distance(a: Distance, b: Distance):
-	return a.distance < b.distance
+	_distances.sort_custom(_sort_by_distance)
 	
 func _sort_by_circuit_len_descending(a: Circuit, b: Circuit):
 	return len(a.members) > (len(b.members))
 	
 func _connect_nearest_to_circuit(n_nearest: int = 0) -> void:
-	_distances.sort_custom(_sort_by_distance)
 	if !n_nearest:
 		n_nearest = 10000000000000
 	for i in range(0, n_nearest):
@@ -101,16 +117,19 @@ func _connect_nearest_to_circuit(n_nearest: int = 0) -> void:
 			c.members = [point]
 			point.circuit = c
 			_circuits.append(c)
+	
+	_circuits.sort_custom(_sort_by_circuit_len_descending)
 
 func _ready() -> void:
+	var start_time = Time.get_ticks_msec()
 	_compute_distances(_junction_positions)
 	_connect_nearest_to_circuit(1000)
-	_circuits.sort_custom(_sort_by_circuit_len_descending)
 	var result1: int = 1
 	for i in 3:
 		result1 *= len(_circuits[i].members)
 	print("Result1: " + str(result1))
 	_connect_nearest_to_circuit()
-	
+	var elapsed = Time.get_ticks_msec() - start_time
+	print("Elapsed: ", elapsed, " ms")
 	
 		
